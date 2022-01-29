1b0bf32d9731194e3b65704b09f047e819597564
mtauhidul <mislam.tauhidul@gmail.com>
2022-01-30 04:04:56 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,138 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Form from './Form';
+
+const regions = [
+  'Central US',
+  'East US',
+  'East US 2',
+  'South Central US',
+  'West US 2',
+  'West US 3',
+];
+
+const resourceTypes = [
+  {
+    type: 'App service',
+    name: 'App service',
+  },
+  {
+    type: 'Function app',
+    name: 'Function app',
+  },
+  {
+    type: 'Key Vault',
+    name: '',
+  },
+  {
+    type: 'Front Door',
+    name: '',
+  },
+  {
+    type: 'Application Gateway',
+    name: '',
+  },
+  {
+    type: 'Cosmos Database (sql)',
+    name: 'Cosmos Database (sql)',
+  },
+  {
+    type: 'Cosmos DB (mongo)',
+    name: '',
+  },
+  {
+    type: 'Sql database Service Bus',
+    name: 'Sql database Service Bus',
+  },
+  {
+    type: 'Synapse workspace',
+    name: '',
+  },
+  {
+    type: 'Data Factory',
+    name: '',
+  },
+  {
+    type: 'Storage account',
+    name: 'Storage account',
+  },
+  {
+    type: 'Machine Learning Workspace',
+    name: '',
+  },
+];
 
 const App = () => {
-  return <div></div>;
+  const [projects, setProjects] = useState([]);
+  const [resources, setResources] = useState([]);
+  useEffect(() => {
+    var config = {
+      method: 'get',
+      url: 'https://dev.azure.com/AllITConsultants/_apis/projects?api-version=6.0',
+      headers: {
+        Authorization:
+          'Basic OjI0dTJ6dXUzaHk1eXZxZm96c3FobWszdms3cGt2eWxiYTZzeDR4d2xhcnlhM2ZkbmVxd3E=',
+      },
+    };
+
+    const getProjects = async () => {
+      await axios(config)
+        .then(function (response) {
+          setProjects(response.data.value);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    getProjects();
+  }, []);
+
+  const postData = async (data) => {
+    const today = new Date();
+    const filename =
+      'post' +
+      '_' +
+      today.getDate() +
+      '' +
+      (today.getMonth() + 1) +
+      '' +
+      today.getFullYear().toString().substr(-2) +
+      '.' +
+      today.getHours() +
+      today.getMinutes();
+
+    var config = {
+      method: 'put',
+      url: `https://terraformdemostr.blob.core.windows.net/form/${filename}?sp=rac&st=2022-01-29T21:47:08Z&se=2022-01-30T05:47:08Z&spr=https&sv=2020-08-04&sr=c&sig=vhZ0FW%2FQGgwb1nP37MHhBddcZ98qQzt%2FIPKRSR96LK8%3D`,
+      headers: {
+        'x-ms-blob-type': 'BlockBlob',
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
+    await axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div id='app'>
+      <Form
+        projects={projects}
+        regions={regions}
+        resources={resources}
+        setResources={setResources}
+        resourceTypes={resourceTypes}
+        postData={postData}
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Form = ({
+  postData,
+  projects,
+  regions,
+  resources,
+  setResources,
+  resourceTypes,
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    data = { ...data, 'Resource Types': resources };
+    postData(data);
+  };
+  if (errors.length > 0) {
+    console.log(errors);
+  }
+
+  const getResources = (idx) => {
+    const value = resourceTypes[idx];
+    const duplicate = resources.find(
+      (resource) => resource.type === value.type
+    );
+    if (!duplicate) {
+      setResources([...resources, value]);
+    }
+  };
+
+  const addDuplicateResource = (input) => {
+    const value = input.type.replace(/[0-9]/g, '');
+    const duplicates = resources.filter((resource) => {
+      const target = value.split(' ');
+      return resource.type.includes(target[0] + ' ' + target[1]);
+    });
+    if (duplicates.length > 0) {
+      setResources([
+        ...resources,
+        {
+          type: `${value} ${duplicates.length + 1}`,
+          name: `${value} ${duplicates.length + 1}`,
+        },
+      ]);
+    }
+  };
+  const handleNameEdit = (item, value) => {
+    const target = resources.find((resource) => resource === item);
+    const index = resources.indexOf(target);
+    resources[index].name = value;
+    setResources(resources);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        required
+        type='text'
+        placeholder='Application name'
+        {...register('Application name', { required: true })}
+      />
+      <select
+        required
+        {...register('Azure devops project', { required: true })}>
+        {projects.map((project, index) => {
+          return (
+            <option key={index} value={project.name}>
+              {project.name}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        required
+        type='text'
+        placeholder='Application DNS name'
+        {...register('Application DNS name', { required: true })}
+      />
+      <input
+        required
+        type='email'
+        placeholder='Owner email'
+        {...register('Owner email', { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <select required {...register('Azure region', { required: true })}>
+        {regions.map((region, index) => {
+          return (
+            <option key={index + 20} value={region}>
+              {region}
+            </option>
+          );
+        })}
+      </select>
+
+      <select
+        required
+        onChange={(e) => {
+          getResources(e.target.value);
+        }}>
+        {resourceTypes.map((resource, index) => {
+          return (
+            <option key={index + 30} value={index}>
+              {resource.type}
+            </option>
+          );
+        })}
+      </select>
+
+      <div id='resources'>
+        {resources.map((resource, index) => {
+          return (
+            <div key={index} id='resourceContainer'>
+              <div id='resource'>
+                <small>{resource.type}</small>
+                {resource.type.includes('Storage account') ||
+                resource.type.includes('Sql database Service Bus') ||
+                resource.type.includes('Cosmos Database (sql)') ||
+                resource.type.includes('Function app') ||
+                resource.type.includes('App service') ? (
+                  <button
+                    onClick={() => {
+                      addDuplicateResource(resource);
+                    }}>
+                    +
+                  </button>
+                ) : null}
+              </div>
+              <div>
+                {resource.type.includes('Storage account') ||
+                resource.type.includes('Sql database Service Bus') ||
+                resource.type.includes('Cosmos Database (sql)') ||
+                resource.type.includes('Function app') ||
+                resource.type.includes('App service') ? (
+                  <input
+                    onChange={(e) => {
+                      handleNameEdit(resource, e.target.value);
+                    }}
+                    id='nameEditor'
+                    type='text'
+                    placeholder={'Enter Name'}
+                  />
+                ) : (
+                  <div style={{ Width: '80%' }} />
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <br />
+      <input type='submit' />
+    </form>
+  );
+};
+
+export default Form;
